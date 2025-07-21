23486b0a3087661137bd42b9e204873b36bed869
MostafaSewidan <sewidanmostafa@gmail.com>
2025-07-21 23:09:21 +0300

    Correct file paths in HlsVideoServiceProvider to reflect updated directory structure.

--- src/Providers/HlsVideoServiceProvider.php
+++ src/Providers/HlsVideoServiceProvider.php
@@ -10,23 +10,23 @@ class HlsVideoServiceProvider extends ServiceProvider
     {
         // Config
         $this->publishes([
-            __DIR__.'/../../Config/hls-videos.php' => config_path('hls-videos.php'),
+            __DIR__.'/../Config/hls-videos.php' => config_path('hls-videos.php'),
         ], 'config');
 
         // Views
-        $this->loadViewsFrom(__DIR__.'/../../Resources/views', 'hls-videos');
+        $this->loadViewsFrom(__DIR__.'/../Resources/views', 'hls-videos');
 
         // Routes
-        $this->loadRoutesFrom(__DIR__.'/../../Routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../Routes/web.php');
 
         // Migrations
-        $this->loadMigrationsFrom(__DIR__.'/../../Database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/migrations');
     }
 
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../../Config/hls-videos.php', 'hls-videos'
+            __DIR__.'/../Config/hls-videos.php', 'hls-videos'
         );
     }
 }
