a7a044287b2c17c6b9356a2fede2c8ac14d76d91
MostafaSewidanTocaan <M.swedan@tocaan.com>
2026-06-06 18:01:05 +0300

    feat(VideoService): add support for ignored domains in video URL replacement
    - Enhanced the VideoService to replace domains listed in the ignored domains configuration with the application URL during video content processing.
    - This improvement allows for better handling of video URLs that may originate from various domains, ensuring consistent streaming behavior.

--- src/Services/VideoService.php
+++ src/Services/VideoService.php
@@ -252,6 +252,12 @@ class VideoService
             $oldTsFilesUrl = route(config('hls-videos.access_route_stream'), [$videoId, $quality]);
             $newTsFilesUrl = config('hls-videos.stream_disk_url')."/$videoId/{$quality}";
             $content = str_replace($oldTsFilesUrl, $newTsFilesUrl, $content);
+            if(config('hls-videos.ignored_domains') && is_array(config('hls-videos.ignored_domains')) &&count(config('hls-videos.ignored_domains')) > 0) {
+                foreach (config('hls-videos.ignored_domains') as $domain) {
+                    $url = str_replace($domain, env('APP_URL'), $url);
+                    $content = str_replace($url, $newTsFilesUrl, $content);
+                }
+            }
         }
         return response($content, 200, [
             'Content-Type' => $contentType,
